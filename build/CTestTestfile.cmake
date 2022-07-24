9a44631962f04b21521d5194e7b43c5cf9cc3793
Lukasz Surmial <surmiak.lukasz86@gmail.com>
2022-07-24 15:22:56 +0000

    cleaning

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/odroid/workspace/c_cpp/sefl_driving_robot
-# Build directory: /home/odroid/workspace/c_cpp/sefl_driving_robot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
